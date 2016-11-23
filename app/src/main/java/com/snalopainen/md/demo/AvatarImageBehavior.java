b14887fcb033f48dcb384b5d0e97ab3f6aa37bff
snajdan890525 <435063994@qq.com>
2016-11-23 12:40:46 +0800

    add AvatarImageBehavior

---
+++ app/src/main/java/com/snalopainen/md/demo/AvatarImageBehavior.java
@@ -0,0 +1,13 @@
+package com.snalopainen.md.demo;
+
+import android.support.design.widget.CoordinatorLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by snajdan on 2016/11/23.
+ */
+
+public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageView> {
+
+}
