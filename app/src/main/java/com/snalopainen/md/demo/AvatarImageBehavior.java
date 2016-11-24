cfe361a524f87e9bc2f3905a5b4d978af4bfa23e
snajdan890525 <435063994@qq.com>
2016-11-24 13:32:00 +0800

    add styleable

--- app/src/main/java/com/snalopainen/md/demo/AvatarImageBehavior.java
+++ app/src/main/java/com/snalopainen/md/demo/AvatarImageBehavior.java
@@ -1,6 +1,11 @@
 package com.snalopainen.md.demo;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -9,5 +14,28 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 
 public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageView> {
+    private Context mContext;
+    private float mCustomFinalYPosition;
+    private float mCustomStartXPosition;
+    private float mCustomStartToolbarPosition;
+    private float mCustomStartHeight;
+    private float mCustomFinalHeight;
 
+    public AvatarImageBehavior(Context context, AttributeSet attrs) {
+        mContext = context;
+        if (attrs != null) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.AvatarImageBehavior);
+            mCustomFinalYPosition = ta.getDimension(R.styleable.AvatarImageBehavior_finalYPosition, 0);
+            mCustomStartXPosition = ta.getDimension(R.styleable.AvatarImageBehavior_startXPosition, 0);
+            mCustomStartToolbarPosition = ta.getDimension(R.styleable.AvatarImageBehavior_startToolbarPosition, 0);
+            mCustomStartHeight = ta.getDimension(R.styleable.AvatarImageBehavior_startHeight, 0);
+            mCustomFinalHeight = ta.getDimension(R.styleable.AvatarImageBehavior_finalHeight, 0);
+            ta.recycle();
+        }
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, CircleImageView child, View dependency) {
+        return dependency instanceof Toolbar;
+    }
 }
